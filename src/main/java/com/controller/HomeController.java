447e9c862fd8ee8042edd791a8c130fe9815f604
Matthew Snaith <mathswan1984@gmail.com>
2017-02-24 13:57:00 +0000

    code refactor

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,18 +1,16 @@
-package com.mkyong.common.controller;
+package com.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloWorldController extends AbstractController{
+public class HomeController extends AbstractController{
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) {
-
-        ModelAndView model = new ModelAndView("HelloWorldPage");
+        ModelAndView model = new ModelAndView("index");
         model.addObject("msg", "hello world");
-
         return model;
     }
 }
